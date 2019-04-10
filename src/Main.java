b59c9992cae7c40da89c1a84adbc0c0c42889347
natalia <natalia@Users-Mac-mini.local>
2019-04-10 15:36:29 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+
+    public static void main(String[] args) {
+        boolean b = true;
+        char c = 'a';
+        int i = 7;
+        long l = 40;
+        float f = 4.7f;
+        double d = 4.43;
+        String s = "abc";
+        int a1=10;
+        int a2=2;
+        int suma=a1+a2;
+        int roznica = a1-a2;
+        int iloczyn = a1*a2;
+        int iloraz=a1/a2;
+
+        System.out.println("Hello World!");
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(s);
+        System.out.println("dodawanie: "+suma);
+        System.out.println("odejmowanie: "+roznica);
+        System.out.println("mnozenie: "+iloczyn);
+        System.out.println("dzielenie: "+iloraz);
+
+    }
+}
