d3eff9264204fd9d86fe6820f94470e36364853b
nataliagilewska <n.gilewska@tt.com.pl>
2019-04-11 23:05:11 +0200

    nowa klasa Celsjusz

---
+++ src/Celsjusz.java
@@ -0,0 +1,16 @@
+public class Celsjusz {
+    double celsjusz;
+    Celsjusz(double celsjusz)
+    {
+        this.celsjusz=celsjusz;
+    }
+
+    public double celsjuszeNaFahrenheity()
+    {
+
+        return celsjusz*1.8+32;
+    }
+
+
+
+}
